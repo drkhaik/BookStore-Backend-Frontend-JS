b4fa6f0b7bc7a8ea603f677f95f46444126734cc
drkhaikDev <trinhkhaiviet@gmail.com>
2023-07-24 18:06:03 +0700

    int project

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../redux/counter/counterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
